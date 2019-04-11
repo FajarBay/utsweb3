7dd69b4f9d4261e2a7a447fe4f0f072576307d75
FajarBay <fajarbay@github.com>
2019-04-11 08:37:20 +0700

    Updates

---
+++ precache-manifest.709bbf89f78a4e30a8fc939edf36ec5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "cf56ebcf01d8b7e43e0d",
+    "url": "/utsweb3/static/js/main.1f0c3079.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/utsweb3/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cf56ebcf01d8b7e43e0d",
+    "url": "/utsweb3/static/css/main.af0061a0.chunk.css"
+  },
+  {
+    "revision": "e5ddcc35c8d023220feeda8e5696dfaf",
+    "url": "/utsweb3/index.html"
+  }
+];
